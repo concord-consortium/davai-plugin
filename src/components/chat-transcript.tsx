c0c50c4529eb9b517134fce3b79b3462bc35ae46
Ethan McElroy <emcelroy@concord.org>
2024-11-18 16:34:39 -0500

    chore: refactor markup

--- src/components/chat-transcript.tsx
+++ src/components/chat-transcript.tsx
@@ -19,32 +19,41 @@ export const ChatTranscriptComponent = ({chatTranscript}: IProps) => {
   });
 
   return (
-    <ul
-      aria-label="DAVAI Chat Transcript"
-      className="chat-transcript"
-      data-testid="chat-transcript"
-      role="main"
-    >
-      {chatTranscript.messages.map((message: ChatMessage) => {
-        return (
-          <li
-            aria-label={`${message.speaker} at ${message.timestamp}`}
-            // For now we are using "assertive" and only applying aria-live to AI messages. This
-            // may change as we refine the experience.
-            aria-live={message.speaker === "DAVAI" ? "assertive" : undefined}
-            className="chat-transcript__message"
-            data-testid="chat-message"
-            key={message.timestamp}
-          >
-            <h2 aria-label="speaker" data-testid="chat-message-speaker">
-              {message.speaker}
-            </h2>
-            <p aria-label="message" data-testid="chat-message-content">
-              {message.content}
-            </p>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="chat-transcript-container" role="main">
+      <p id="chat-transcript-description" className="visually-hidden">
+        This is a transcript of a chat with DAVAI.
+      </p>
+      <div
+        aria-label="DAVAI Chat Transcript"
+        aria-describedby="chat-transcript-description"
+        className="chat-transcript"
+        contentEditable="true"
+        data-testid="chat-transcript"
+        // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+        tabIndex={0}
+        onBeforeInput={(e) => e.preventDefault()}
+      >
+        {chatTranscript.messages.map((message: ChatMessage) => {
+          return (
+            <div
+              aria-label={`${message.speaker} at ${message.timestamp}`}
+              // For now we are using "assertive" and only applying aria-live to AI messages. This
+              // may change as we refine the experience.
+              aria-live={message.speaker === "DAVAI" ? "assertive" : undefined}
+              className="chat-transcript__message"
+              data-testid="chat-message"
+              key={message.timestamp}
+            >
+              <h2 aria-label="speaker" data-testid="chat-message-speaker">
+                {message.speaker}
+              </h2>
+              <p aria-label="message" data-testid="chat-message-content">
+                {message.content}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
