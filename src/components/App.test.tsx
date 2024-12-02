53b4a4a910414954a77e7d817e60402d45d83fa3
lublagg <lvblagg@gmail.com>
2024-12-02 15:14:03 -0500

    fix failing test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,31 +1,17 @@
 import React from "react";
-import "openai/shims/node";
-import { App } from "./App";
 import { render, screen } from "@testing-library/react";
+import { App } from "./App";
 
-const mockOpenAi = jest.fn(
-  async () => ({
-    id: "assistant-id",
-  })
-);
-
-jest.mock("../utils/llm-utils", () => ({
-  initLlmConnection: () => ({
-    beta: {
-      assistants: {
-        create: mockOpenAi
-      },
-      threads: {
-        create: jest.fn()
-      }
-    }
-  }),
-  getTools: jest.fn()
+jest.mock("../models/assistant-model", () => ({
+  assistantStore: {
+    assistant: null,
+    initialize: jest.fn(),
+  },
 }));
 
 describe("test load app", () => {
-  it("renders without crashing", () => {
-    render(<App/>);
-    expect(screen.getByText("Loading...")).toBeDefined();
+  it("renders without crashing", async() => {
+    render(<App />);
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
   });
 });
