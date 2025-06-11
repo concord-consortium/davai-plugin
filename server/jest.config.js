a135306c0ae6bf3e7cc45eb9e22c39e25e04c15f
lublagg <lvblagg@gmail.com>
2025-06-11 13:13:19 -0400

    try cursor suggestions

--- server/jest.config.js
+++ server/jest.config.js
@@ -11,15 +11,21 @@ const transpileModules = [
 export default {
   coveragePathIgnorePatterns: ["/node_modules/"],
   moduleFileExtensions: ["ts", "js", "json"],
-  // moduleNameMapper: {
-  //   "^(\\.{1,2}/.*)\\.js$": "$1"
-  // },
-  preset: "ts-jest/presets/js-with-ts",
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   testPathIgnorePatterns: ["/node_modules/"],
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true
+    }
+  },
   transform: {
-    "^.+\\.(js)$": "babel-jest"
+    "^.+\\.(js)$": "babel-jest",
+    "^.+\\.ts$": ["ts-jest", {
+      useESM: true
+    }]
   },
   transformIgnorePatterns: [
     `/node_modules/(?!${transpileModules.join("|")}/)`
