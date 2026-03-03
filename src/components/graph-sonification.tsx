bb09619f2ab37752cd75660eb2494055c80ce242
Ethan McElroy <emcelroy@concord.org>
2026-03-03 14:34:32 -0500

    fix: Don't highlight play button when clicked (DAVAI-103)

--- src/components/graph-sonification.tsx
+++ src/components/graph-sonification.tsx
@@ -36,6 +36,7 @@ export const GraphSonification = observer(() => {
   const playPauseButtonRef = useRef<HTMLButtonElement>(null);
 
   const [showError, setShowError] = useState(false);
+  const [keyboardFocused, setKeyboardFocused] = useState(false);
 
   const selectedGraphID = selectedGraph?.id;
 
@@ -108,6 +109,7 @@ export const GraphSonification = observer(() => {
     return shortcutsService.registerShortcutHandler("sonifyGraph", (event) => {
       event.preventDefault();
 
+      setKeyboardFocused(true);
       playPauseButtonRef.current?.focus();
       playPauseButtonRef.current?.scrollIntoView({behavior: "smooth", block: "nearest"});
     }, { focus: true });
@@ -166,9 +168,10 @@ export const GraphSonification = observer(() => {
       <div className="sonification-buttons">
         <button
           ref={playPauseButtonRef}
-          className="play"
+          className={`play${keyboardFocused ? " keyboard-focused" : ""}`}
           data-testid="playback-button"
           onClick={handlePlayPause}
+          onBlur={() => setKeyboardFocused(false)}
           aria-disabled={!selectedGraphID}
         >
           { isPlaying ? <PauseIcon /> : <PlayIcon /> }
